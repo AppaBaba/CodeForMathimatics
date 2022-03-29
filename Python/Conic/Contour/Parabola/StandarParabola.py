611c241a63117ed5512736f49ed8c46b2bbb493c
darleg <darleg@outlook.com>
2022-03-29 16:31:23 -0700

    replace parabola programs

--- Python/Conic/Contour/Parabola/StandarParabola.py
+++ Python/Conic/Contour/Parabola/StandarParabola.py
@@ -1,11 +1,9 @@
 from numpy import linspace, meshgrid
-from pylab import figure, subplots, axhline, axvline, contour, title, show
+from pylab import figure, subplots, axhline, axvline, contour, show
 
-# standar equation of a parabola
-# y=ax^2 + bx + c
-# vertex equation of a parabola
-# y=a(x-h)^2 + k
-
+# Standar Parabola Form
+# vertex at the origin
+#
 
 def axis():
     axhline(0, alpha=.2, c = 'c')
@@ -19,27 +17,7 @@ def yaparabola(a):
     yap = x**2 - 4*a*y
     return yap
 
-def pyparabola(a,b,c):
-    py = -y + a*x**2 + b*x + c
-    return py
-
-def PlotYAParabola(a,b,c,s,t):
-    axis()
-    YAP = pyparabola(a,b,c)
-    contour(x, y, YAP, [0], colors = s)
-    title(t)
-    
-def pxparabola(a,b,c):
-    px = -x + a*y**2 + b*y + c
-    return px
-
-def PlotXAParabola(a,b,c,s,t):
-    axis()
-    XAP = pxparabola(a,b,c)
-    contour(x, y, XAP, [0], colors = s)
-    title(t)
-
-x = linspace(-5, 5, 200)
+x = linspace(-8, 8, 200)
 y = linspace(-5, 5, 200)
 x, y = meshgrid(x, y)
 
@@ -47,37 +25,29 @@ fig = figure()
 
 ax = fig.add_subplot(2, 2, 1)
 
-a = 1
-b = -2
-c = -3
-s = 'r'
-t = 'Standar Y-Axis Parabola'
-PlotYAParabola(a,b,c,s,t)
+a = .4
+axis()
+xap = xaparabola(a)
+contour(x, y, xap, [0], colors='r')
 
 ax = fig.add_subplot(2, 2, 2)
 
-a = 1
-b = -2
-c = -2
-s = 'g'
-t = 'Standar X-Axis Parabola'
-PlotXAParabola(a,b,c,s,t)
+a = .4
+axis()
+yap = yaparabola(a)
+contour(x, y, yap, [0], colors='g')
 
 ax = fig.add_subplot(2, 2, 3)
 
-a = -2
-b = 3
-c = 2
-s = 'b'
-t = 'Standar Y-Axis Parabola'
-PlotYAParabola(a,b,c,s,t)
+a = -.4
+axis()
+xap = xaparabola(a)
+contour(x, y, xap, [0], colors='b')
 
 ax = fig.add_subplot(2, 2, 4)
-a = -2
-b = 3
-c = 2
-s = 'm'
-t = 'Standar X-Axis parabola'
-PlotXAParabola(a,b,c,s,t)
+a = -.4
+axis()
+yap = yaparabola(a)
+contour(x, y, yap, [0], colors='m')
 
 show()
