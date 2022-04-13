c6aed61fffeb94fc0b065aee5734c61fbb49a6b1
darleg <darleg@outlook.com>
2022-04-12 20:27:54 -0700

    add plots

---
+++ Python/Derivative/DerivativePlot/DevPlot02.py
@@ -0,0 +1,24 @@
+from matplotlib.pyplot import plot, legend, grid, suptitle, show
+from scipy.misc import derivative
+from numpy import linspace
+
+# f(x)
+def f(x):
+	return x**5
+# d/dx
+def d(x):
+	return derivative(f, x)
+
+# x-axis
+y = linspace(-10, 10)
+
+# plot f(x)
+plot(y, f(y), color='g', label='f(x)')
+# plot d/dx
+plot(y, d(y), color='m', label='d/dx')
+
+# plot format
+legend(loc='lower right')
+grid(True)
+suptitle('f(x) = x^5')
+show()
