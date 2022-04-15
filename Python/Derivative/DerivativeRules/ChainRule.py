26c0d44dc2c2952c7fb3710aa71844d53a88c4cb
darleg <darleg@outlook.com>
2022-04-15 09:39:15 -0700

    update  rulses

--- Python/Derivative/DerivativeRules/ChainRule.py
+++ Python/Derivative/DerivativeRules/ChainRule.py
@@ -6,5 +6,6 @@ x = Symbol('x')
 # let f(x) = cos(x) and g(x) = x^2
 # then cos(x^2) = f(g(x))
 fx = cos(x**2)
+print('f(x) = cos(x^2)')
 # d/dx[f(g(x)) = f'(g(x))g'(x)
 print('d/dx cos(x^2) =', diff(fx))
--- Python/Derivative/DerivativeRules/ProductRule.py
+++ Python/Derivative/DerivativeRules/ProductRule.py
@@ -6,5 +6,7 @@ x = Symbol('x')
 # or    [fg]' = f'g + fg'
 
 # f(x) = exp(x) * cos(x)
-fx = exp(x)*cos(x)
+fx = exp(x) * cos(x)
+print('f(x) = exp(x) * cos(x)')
+# f'(x) = f'(exp(x)g(cos(x) + f(exp)g'(cos(x)
 print('d/dx exp(x) * cos(x) =', diff(fx))
